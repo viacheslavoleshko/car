562ff1d6529d53717931b13364bdc8cc9b47fa1b
NotProf <igor.karpyuk00@gmail.com>
2019-08-19 18:17:53 +0300

    CAR-15: millage graph

--- ionic/src/app/purchase.service.ts
+++ ionic/src/app/purchase.service.ts
@@ -20,7 +20,7 @@ export class PurchaseService {
 
     confirm(id, regNumb, paymentIntent) {
         let headers = new HttpHeaders().set('token',id).set('regNumb', regNumb).set('paymentIntent',paymentIntent );
-        return this.http.get('https://car.nix.ltd/confirm', {headers: headers});
+        return this.http.get('https://car.hpcheck.co.uk/confirm', {headers: headers});
     }
 
 }
