84e561b462dde28ed035c5ecab182900a365f73e
Vyacheslav Oleshko <vyacheslav.oleshko.work@gmail.com>
2019-08-25 17:34:11 +0300

    CAR-44 : Cars with lowest tax

--- laravel/app/Http/Controllers/LowTaxController.php
+++ laravel/app/Http/Controllers/LowTaxController.php
@@ -8,14 +8,18 @@ use App\Models\Mot;
 
 class LowTaxController extends Controller
 {
-    public function index($order = 'model')
+    public function index()
     {
         return response()->json([
             'object' => Mot::select('make', 'model', 'year', 'tax12_single')
-                ->join('year_tax', 'co2', '=', 'mot.co2')
-                ->whereRaw('mot.co2 between year_tax.co_min and year_tax.co_max')
-                ->whereBetween('mot.co2', [0, 150])
-                ->orderBy($order)
+                ->join('year_tax', function ($join) {
+                    $join->whereRaw('mot.co2 between year_tax.co_min and year_tax.co_max');
+                })
+                ->whereBetween('mot.co2', [0, 120])
+                ->whereNotNull('make')
+                ->distinct()
+                ->orderByRaw('make, model, year')
+                ->take(100)
                 ->get(),
         ]);
     }
