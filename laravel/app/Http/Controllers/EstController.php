f413dac5cb8ba046a3f18c1e58cabd133d0a7d9a
NotProf <igor.karpyuk00@gmail.com>
2019-08-27 18:38:17 +0300

    CAR-7: Car Estimate

--- laravel/app/Http/Controllers/EstController.php
+++ laravel/app/Http/Controllers/EstController.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: IK
- * Date: 26.08.2019
- * Time: 13:07
- */
+
 
 namespace App\Http\Controllers;
 
@@ -12,6 +7,7 @@ namespace App\Http\Controllers;
 use App\Models\Est;
 use App\Models\Mot;
 use Illuminate\Http\Request;
+use DB;
 
 
 class EstController extends Controller
@@ -22,4 +18,46 @@ class EstController extends Controller
           'est' => Est::select('*')->whereNotNull('price')->where('make',$mot['m']['make'])->where('model', $mot['m']['model'])->first()
       ]);
   }
+    public function estimate() {
+        $apikey = getenv('VDI_API_TEST');
+        $records = DB::select(DB::raw("select min(reg), make, model from (
+       select reg, m.make, m.model from mot m
+       left join (select distinct make, model from est) e
+       on m.make = e.make and m.model = e.model
+       where reg like '%A%'
+       and m.m is not null
+       and m.m::text <> '-1'
+       and m.make is not null
+       and m.model is not null
+       and m.estimate is null
+       and e.make is null
+       ) t
+          group by make, model
+          LIMIT 10
+        "));
+
+
+        foreach ($records as $record) {
+            $info = "https://uk1.ukvehicledata.co.uk/api/datapackage/ValuationData?v=2&api_nullitems=1&auth_apikey={$apikey}&key_VRM={$record->min}";
+            $apiResult = file_get_contents($info);
+            $res = $apiResult ? $apiResult : null;
+            //sleep(3);
+            $res = stripslashes(html_entity_decode($res));
+            $estimate = json_decode($res, true);
+            $otr = array_column($estimate['Response']['DataItems'], 'OTR');
+            Mot::where('reg', $record->min)->update([
+                'estimate' => $res
+            ]);
+
+            DB::table('est')->insert([
+                ['make' => $record->make,
+                    'model' => $record->model,
+                    'price' => $otr[0]]
+            ]);
+
+        }
+         echo json_encode([
+             'status' => 'OK'
+         ]);
+    }
 }
--- laravel/app/Http/Controllers/MotController.php
+++ laravel/app/Http/Controllers/MotController.php
@@ -77,7 +77,7 @@ class MotController extends Controller
         return $result;
     }
 
-    function regsFromPage($pages) 
+    function regsFromPage($pages)
     {
         $start = microtime(true);
         set_time_limit(0);
@@ -87,12 +87,12 @@ class MotController extends Controller
 
         for($i = 0; $i < $pages; $i++) {
             $url = "https://beta.check-mot.service.gov.uk/trade/vehicles/mot-tests?page=$i";
-            $apiKey = getenv('MOT_API');   
+            $apiKey = getenv('MOT_API');
             $headers = array(
                 'Accept: application/json+v6',
                 "X-Api-Key: $apiKey"
             );
-        
+
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_URL, $url);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -120,16 +120,16 @@ class MotController extends Controller
                         ];
                         array_push($valid, $record);
                     }
-                }        
+                }
             }
-            
+
             array_push($data, $valid);
             array_unique($data);
             $result = array_merge(...$data);
             sleep(2);
         }
         curl_close($ch);
-        
+
         $result = collect($result);
         $chunks = $result->chunk(1000);
 
@@ -151,19 +151,5 @@ class MotController extends Controller
         return  Mot::select('m')->where('reg', $number)->first();
     }
 
-    public function estimate() {
-        $apikey = getenv('VDI_API');
-        $records = Mot::select('*')->whereNotNull('m')->whereNull('estimate')->where('reg', 'like', '%A%')->get();
-        foreach ($records as $record) {
-            $info = "https://uk1.ukvehicledata.co.uk/api/datapackage/ValuationData?v=2&api_nullitems=1&auth_apikey={$apikey}&key_VRM={$record->reg}";
-            $res = file_get_contents($info) ? file_get_contents($info): null;
-            sleep(1);
-            Mot::where('reg', $record->reg)->update([
-                'estimate' => $res
-            ]);
-            echo json_encode([
-                'response' => $res
-            ]);
-        }
-    }
 }
+
