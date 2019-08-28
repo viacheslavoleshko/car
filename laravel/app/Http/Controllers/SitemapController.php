a371dbc7f1e214de7003fb1152178e3c48950ca6
Vyacheslav Oleshko <vyacheslav.oleshko.work@gmail.com>
2019-08-28 20:59:00 +0300

    Sitemaps fix

--- laravel/app/Http/Controllers/SitemapController.php
+++ laravel/app/Http/Controllers/SitemapController.php
@@ -5,11 +5,16 @@ use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Request;
 use App\Models\Mot;
 use Illuminate\Support\Facades\Response;
+use Illuminate\Filesystem\Filesystem;
 
 class SitemapController extends Controller
 {
     public function index()
     {
+        set_time_limit(0);
+        $file = new Filesystem;
+        $file->cleanDirectory(getenv('PWA_LOCATION'). '/www/gmaps/car/');
+
         $count = Mot::whereNotNull('updated_at')->count();
         $style = getenv('APP_URL') . '/sitemap/styles/index.xsl';
         $pages = ceil($count / getenv('SITEMAP_OFFSET'));
@@ -21,12 +26,15 @@ class SitemapController extends Controller
             'style' => $style,
             'pwa' => $pwa,
         ]);
-        Storage::disk('index')->delete(["sitemap.xml"]);
         Storage::disk('index')->put("sitemap.xml", $file);
         
         for($i = 1; $i <= $pages; $i++) {
             self::numbers($i);
+            echo nl2br($i . " page inserted to file from \n");
+            ob_flush();
+            flush();
         }
+        ob_end_flush();
     }
 
     public function numbers($page)
@@ -47,7 +55,6 @@ class SitemapController extends Controller
                 'style' => $style,
                 'pwa' => $pwa,
             ]);
-            Storage::disk('car')->delete(["$page.xml"]);
             Storage::disk('car')->put("$page.xml", $file);
         } else {
             abort(404);
